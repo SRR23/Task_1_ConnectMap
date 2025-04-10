c32f44b994c7fef9e53e2cb9381f9759c98c07ba
SRR23 <srrimon267@gmail.com>
2025-04-10 13:16:16 +0600

    Update MyMapV14

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 
-
-import MyMapV10 from "./componenet/MyMapV10";
-import MyMapV11 from "./componenet/MyMapV11";
-import MyMapV12 from "./componenet/MyMapV12";
 import MyMapV13 from "./componenet/MyMapV13";
-import MyMapV9 from "./componenet/MyMapV9";
-import Polygon4 from "./practice/Polygon4";
+import MyMapV14 from "./componenet/MyMapV14";
 import Polygon5 from "./practice/Polygon5";
 
 
@@ -13,13 +8,11 @@ function App() {
   return (
     <>
       
-      {/* <Polygon4 /> */}
-      {/* <MyMapV10 /> */}
-      {/* <MyMapV11 /> */}
-      {/* <MyMapV12 /> */}
-      <MyMapV13 />
-      {/* <MyMapV9 /> */}
-      {/* <Polygon4 /> */}
+      
+      {/* <MyMapV13 /> */}
+      <MyMapV14 />
+      
+      
       {/* <Polygon5 /> */}
       
     </>
